d3e2d24bcb4a833779ee56682578f04fec4ee169
pressy4pie <konnorrigby@github.com>
2014-04-10 16:21:30 -0700

    fixed root 4 realzies this time i think

--- src/com/pressy4pie/f6utilities2/RooterActivity.java
+++ src/com/pressy4pie/f6utilities2/RooterActivity.java
@@ -9,7 +9,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.util.Log;
@@ -17,17 +19,104 @@ import android.view.Menu;
 import android.view.View;
 
 public class RooterActivity extends Activity {
+	ProgressDialog barProgressDialog;
+	Handler updateBarHandler;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rooter);
+		updateBarHandler = new Handler();
+
 		
         File dir = new File ("/data/data/com.pressy4pie.f6utilities2/saferoot");
         dir.mkdirs();
-        
+        //copy the root files
         CopyAssets();
+		root_tools.executeAsSH("chmod 0755 /data/data/com.pressy4pie.f6utilities2/saferoot/getroot_finish.sh");
+		root_tools.executeAsSH("chmod 0755 /data/data/com.pressy4pie.f6utilities2/saferoot/getroot_begin.sh");
+		root_tools.executeAsSH("chmod 0755 /data/data/com.pressy4pie.f6utilities2/saferoot/getroot");
+		//root_tools.executeAsSH("chmod 0755 /data/local/tmp");
 	}
+	
+	public void getRoot(){
+		//root_tools.executeAsSH("");
+		//set up directories && chmod
+		root_tools.executeAsSH("/data/data/com.pressy4pie.f6utilities2/saferoot/getroot_begin.sh");
+		
+		Log.i("getroot", "getroot_begin.sh executed...");
+		root_tools.executeAsSH("cp /data/data/com.pressy4pie.f6utilities2/saferoot/su /data/local/tmp ");
+		root_tools.executeAsSH("cp /data/data/com.pressy4pie.f6utilities2/saferoot/busybox /data/local/tmp ");
+		root_tools.executeAsSH("cp /data/data/com.pressy4pie.f6utilities2/saferoot/getroot /data/local/tmp ");
+		root_tools.executeAsSH("cp /data/data/com.pressy4pie.f6utilities2/saferoot/install-recovery.sh /data/local/tmp ");
+		
+		Log.i("getroot", "getroot and su binaries coppied");
+		
+		
+		//the actual get root
+		root_tools.executeAsSH("cd /data/local/tmp && ./getroot");
+		Log.i("getroot", "getroot executed...");
+		
+		//clean up
+		root_tools.executeAsSH("/data/data/com.pressy4pie.f6utilities2/saferoot/getroot_finish.sh");
+		Log.i("getroot", "getroot_finish.sh executed...");
+		
+	}
+	
+	public void start(View view) {
+		final ProgressDialog RingProgressDialog = ProgressDialog.show(RooterActivity.this, "Please Wait", "Rooting", true);
+		RingProgressDialog.setCancelable(false);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					//stuff goes here...
+					//Thread.sleep(10000);
+					//root_tools.executeAsSH("/data/data/com.pressy4pie.f6utilities2/saferoot/getroot.sh");
+					getRoot();
+				} catch (Exception e) {
+					Log.e("root", "something went wrong");
+				}
+				RingProgressDialog.dismiss();
+			
+		}
+	}).start();	
+	}
+	
+	public void launchBarDialog(View view) {
+		barProgressDialog = new ProgressDialog(RooterActivity.this);
+		
+		barProgressDialog.setTitle("Rooting...");
+		barProgressDialog.setMessage("Root in Progress...");
+		barProgressDialog.setProgressStyle(barProgressDialog.STYLE_HORIZONTAL);
+		barProgressDialog.setProgress(0);
+		barProgressDialog.setMax(20);
+		barProgressDialog.show();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					//time consuming task here
+					while ( barProgressDialog.getProgress() <= barProgressDialog.getMax() ) {
+						Thread.sleep(2000);
+						updateBarHandler.post(new Runnable() {
+						public void run() {	
+								barProgressDialog.incrementProgressBy(2);	
+							}
+						});
+						if ( barProgressDialog.getProgress() == barProgressDialog.getMax() ) {
+							barProgressDialog.dismiss();
+						}
+					}
+				} catch (Exception e) {	
+				}
+			}
+		}).start();
+	}
+
+
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,7 +164,7 @@ public class RooterActivity extends Activity {
 
 
 
-public void start(View view){
+public void startOld(View view){
 	Runtime rt = Runtime.getRuntime();
 		/*
 		Process q = rt.exec(new String("/system/bin/sh chmod 0755 /data/data/com.pressy4pie.f6utilities2/saferoot/getroot.sh"));
@@ -85,6 +174,7 @@ public void start(View view){
 		root_tools.executeAsSH("chmod 0755 /data/data/com.pressy4pie.f6utilities2/saferoot/getroot.sh");
 		root_tools.executeAsSH("/data/data/com.pressy4pie.f6utilities2/saferoot/getroot.sh");
 }
+
 
 }
 
